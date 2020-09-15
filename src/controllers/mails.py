004b8959e75d06a34827407ed4eb212c05954882
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2020-09-15 21:18:35 +0200

    Mail lists: active, archived old, archived recent

--- src/controllers/mails.py
+++ src/controllers/mails.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, render_template
 from src.utils.mail import send_contact_message, send_mail
 from src.models.db import get_db
-from src.models import EmployeeDataAccess, ProjectDataAccess, StudentDataAccess
+from src.models import EmployeeDataAccess, ProjectDataAccess, StudentDataAccess, GuideDataAccess
 
 bp = Blueprint('mails', __name__)
 
@@ -55,29 +55,85 @@ def post_admin_mail():
     return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
 
 
-def mail_lists(receiver, is_student, lists):
+def order_lists(lists):
+    # If roll over action is present, make sure the order is correct
+    # This is important for the active and archived-old lists
+    if 'archived-recent' in lists:
+        lists.remove('archived-recent')
+        lists.append('archived-recent')
+        if 'active' in lists:
+            lists.remove('active')
+            lists.append('active')
+
+
+def mail_lists(receiver, is_student, lists: list):
+    order_lists(lists)
     text = ''
     for list_type in lists:
-        if list_type == 'active':
-            text += active(receiver, is_student)
+        func = locals()[list_type.replace('-', '_')]
+        text += func(receiver, is_student)
     return text
 
 
 def active(receiver, is_student):
-    pass
+    if is_student:
+        return ''
+    projects = GuideDataAccess(get_db()).get_projects_for_employee(receiver.e_id)
+    with ProjectDataAccess(get_db()) as access:
+        projects = [access.get_project(x['project_id'], True) for x in projects]
+    if not projects:
+        return ''
+    text = ''
+    for project in projects:
+        text += project_link(project)
+    return text
 
 
 def archived_recent(receiver, is_student):
-    pass
+    if is_student:
+        return ''
+    projects = GuideDataAccess(get_db()).get_projects_for_employee(receiver.e_id)
+    access = ProjectDataAccess(get_db())
+    projects = [access.get_project(x['project_id'], True) for x in projects]
+    if not projects:
+        return ''
+    text = 'ARCHIVED RECENT'
+    for project in projects:
+        if project_is_full(project):
+            access.set_active(project.project_id, False)
+            text += project_link(project)
+    return text
+
+
+def project_link(project):
+    return f'<a href="https://esp.uantwerpen.be/project-page?project_id={project.project_id}">{project.title}</a>'
+
+
+def project_is_full(project):
+    accepted = 0
+    for registration in project.registrations:
+        if registration['status'] == 'Accepted':
+            accepted += 1
+    return accepted == project.max_students
 
 
 def archived_old(receiver, is_student):
-    pass
+    if is_student:
+        return ''
+    projects = GuideDataAccess(get_db()).get_projects_for_employee(receiver.e_id)
+    access = ProjectDataAccess(get_db())
+    projects = [access.get_project(x['project_id'], False) for x in projects]
+    projects = filter(lambda p: not p.is_active, projects)
+    if not projects:
+        return ''
+    text = 'ARCHIVED OLD'
+    for project in projects:
+        text += project_link(project)
+    return text
 
 
 def projects_assigned_new(receiver, is_student):
     pass
-
 
 def projects_pending(receiver, is_student):
     pass
