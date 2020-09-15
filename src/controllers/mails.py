7185ea964ea37ba41f0bfac3a5d6a303154a61c3
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2020-09-15 18:52:26 +0200

    Admin mail POST endpoint work

--- src/controllers/mails.py
+++ src/controllers/mails.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify, render_template
-from src.utils.mail import send_contact_message
+from src.utils.mail import send_contact_message, send_mail
+from src.models.db import get_db
+from src.models import EmployeeDataAccess, ProjectDataAccess, StudentDataAccess
 
 bp = Blueprint('mails', __name__)
 
@@ -20,12 +22,63 @@ def mail():
 
 
 @bp.route('/admin-mail')
-def admin_mail():
+def view_admin_mail():
     return render_template('mails.html')
 
 
 @bp.route('/admin-mail', methods=['POST'])
 def post_admin_mail():
     data = request.get_json()
+    subject = data['subject']
+    content = data['content']
+    receiver = data['receiver']
+    lists = data['additions']
 
+    is_student = receiver == 'students'
+    if is_student:
+        possible_receivers = StudentDataAccess(get_db()).get_students()
+    else:
+        possible_receivers = EmployeeDataAccess(get_db()).get_employees(False)
+
+    for person in possible_receivers:
+        mail_content = f'Beste {person.name}\n\n{content}'
+        personal_lists = mail_lists(person, is_student, lists)
+        mail_content += personal_lists
+
+        # Don't send mail if not one list is relevant for this person
+        if lists and not personal_lists:
+            continue
+
+        receiver_mail = person.student_id + "@ad.ua.ac.be" if is_student else person.email
+        send_mail(receiver_mail, subject, mail_content)
+
+    return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+def mail_lists(receiver, is_student, lists):
+    text = ''
+    for list_type in lists:
+        if list_type == 'active':
+            text += active(receiver, is_student)
+    return text
+
+
+def active(receiver, is_student):
+    pass
+
+
+def archived_recent(receiver, is_student):
+    pass
+
+
+def archived_old(receiver, is_student):
+    pass
+
+
+def projects_assigned_new(receiver, is_student):
+    pass
+
+
+def projects_pending(receiver, is_student):
+    pass
 
